8d467924b265990d6760ddab1fa4b3c079643c3f
DIvkov575 <Dmitriy@Ivkov.net>
2022-04-17 15:55:39 -0400

    working w/ output page

--- App.py
+++ App.py
@@ -17,9 +17,8 @@ def main():
     return render_template('template-a.html')
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/output', methods=['POST', 'GET'])
 def output():
-
     with open("Assets/sklearn_fit.pkl", 'rb') as weights_file:
         pac = pickle.load(weights_file)
     with open("Assets/tfidf_vectorizer_w_fit.pkl", 'rb') as tfidf_file:
@@ -31,34 +30,8 @@ def output():
     tfidf_out = tfidf_vectorizer.transform([input_1])
     result = pac.predict(tfidf_out)
 
-    # result = 'sd'
+    return render_template('template-b-post-submit.html', input_1=result)
 
-    return render_template('template-a.html', input_1=result)
-
-
-
-
-
-
-# @app.route('/post_screen', methods=['POST', 'GET'])
-# def output():
-#     input_1 = request.form.to_dict()
-#     print(input_1['input_1'], file=sys.stderr)
-#
-#     return render_template('template-b-post-submit.html', input_1=input_1['input_1'])
-
-
-def tmp_ml():
-    input = ""
-    with open("Assets/sklearn_fit.pkl", 'rb') as weights_file:
-        pac = pickle.load(weights_file)
-    with open("Assets/tfidf_vectorizer_w_fit.pkl", 'rb') as tfidf_file:
-        tfidf_vectorizer = pickle.load(tfidf_file)
-
-    tfidf_out = tfidf_vectorizer.transform(input)
-    result = pac.predict(tfidf_out)
-
-    # print(input_1, file=sys.stderr)
 
 if __name__ == '__main__':
     app.run()
