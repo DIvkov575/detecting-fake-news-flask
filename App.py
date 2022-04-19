aa4e503716a403bed5038954b437220f8b7846c1
DIvkov575 <Dmitriy@Ivkov.net>
2022-04-18 22:05:17 -0400

    working

--- App.py
+++ App.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, request
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import PassiveAggressiveClassifier
 import pickle
 import sys
 
@@ -9,28 +7,40 @@ app = Flask(__name__, template_folder='./template/')
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
-    # with open("Assets/sample.pkl", 'r') as weights_file:
-    #     pac = pickle.load(weights_file)
-    # with open("Assets/tfidf_vectorizer_w_fit.pkl", 'r') as tfidf_file:
-    #     tfidf_vectorizer = pickle.load(tfidf_file)
+    global pac, tfidf_vectorizer
+
+    with open("Assets/sklearn_fit.pkl", 'rb') as weights_file:
+        pac = pickle.load(weights_file)
+    with open("Assets/tfidf_vectorizer_w_fit.pkl", 'rb') as tfidf_file:
+        tfidf_vectorizer = pickle.load(tfidf_file)
 
     return render_template('template-a.html')
 
 
 @app.route('/output', methods=['POST', 'GET'])
 def output():
-    with open("Assets/sklearn_fit.pkl", 'rb') as weights_file:
-        pac = pickle.load(weights_file)
-    with open("Assets/tfidf_vectorizer_w_fit.pkl", 'rb') as tfidf_file:
-        tfidf_vectorizer = pickle.load(tfidf_file)
+    global pac, tfidf_vectorizer
 
     input_raw = request.form.to_dict()
     input_1 = input_raw['input_1']
 
-    tfidf_out = tfidf_vectorizer.transform([input_1])
-    result = pac.predict(tfidf_out)
+    # if url get text data from url
+    if input_1.startswith("https://"):
+        import requests
+        from bs4 import BeautifulSoup
+        res = requests.get(input_1)
+        html_page = res.content
+        soup = BeautifulSoup(html_page, 'html.parser')
+        text = soup.get_text()
+        link = input_1
+    else:
+        text = input_1
+        link = None
 
-    return render_template('template-b-post-submit.html', input_1=result)
+    tfidf_out = tfidf_vectorizer.transform([text])
+    result = str(pac.predict(tfidf_out))
+
+    return render_template('template-b-post-submit.html', result=result[2:-2], full_input_text=text, link=link)
 
 
 if __name__ == '__main__':
---
+++ README.md
@@ -0,0 +1,17 @@
+Flask front end for Fake news detector
+---
+Uses a PAC predictor *Github repo: https://github.com/DIvkov575/detecting-fake-news*. All fitting was done in 
+a seperate repo then pickled to be used in this repo. No fitting is done on loading, and loading is done on loading of webpage.
+
+Insert URL or page contents for automatic detection and prediction of content.
+
+Trained on contemporary politcal content so best if such content is used for testing the predictor.
+
+**Fake content link:**
+- https://www.washingtonpost.com/news/fact-checker/wp/2016/08/11/too-good-to-check-sean-hannitys-tale-of-a-trump-rescue/
+- https://www.breitbart.com/politics/2016/09/10/exposed-fbi-director-james-comeys-clinton-foundation-connection/
+- https://www.snopes.com/fact-check/pope-francis-donald-trump-endorsement/
+
+**Real Conent Link:** 
+- https://www.cnn.com/2022/04/18/politics/cdc-mask-mandate-ruling/index.html
+- https://www.axios.com/republicans-inflation-democrats-a69e03c9-2691-4b9c-b18a-b40f0d680e58.html
